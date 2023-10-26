b4f4d30056af82de216092210af3f090b03d081e
Rezo Leshkasheli <rezo_ly@mail.ru>
2023-10-26 18:29:18 +0300

    update 26/10 18:30

---
+++ WEB_REZRABOTKA/src/js/script.js
@@ -0,0 +1,43 @@
+// ПЕРВАЯ СТРАНИЦА!
+
+
+
+
+// let fio = document.querySelector('.name_user_information_right_block');
+// if (document.getElementById("name")!="") {
+//     fio.textContent = document.getElementById("name");
+// } else {
+//     fio.textContent = "NONE";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const dropdowns = document.querySelectorAll('.dropdown_item')
+// dropdowns.forEach(dropdown_item => {
+//     const selected = dropdown_item.querySelector('.selected');
+//     const caret = dropdown_item.querySelector('.caret');
+//     const dropdown_menu = dropdown_item.querySelector('.dropdown_menu');
+//     const options = dropdown_item.querySelector('.dropdown_menu li');
+
+//     selected.addEventistener('click', () => {
+//         selected.classList.toggle('select_clicked');
+//         caret.classList.toggle('caret-rotate');
+//         dropdown_menu.classList.toggle('menu_open');
+//     });
+
+//     options.forEach(option => {
+//         option.addEventistener('click', () => {
+//             // selected.innerText = option.innerText
+//         });
+//     });
+// });
